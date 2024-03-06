96184bb58aded5e38509cec572ca1666ea8a4597
Yoqubjon <yoqubjonabdulboqiyev@gmail.com>
2024-03-06 16:59:46 +0500

    last push

--- src/common/config/index.ts
+++ src/common/config/index.ts
@@ -4,15 +4,10 @@ dotenv.config();
 
 export const config = {
   serverPort: process.env.PORT ? Number(process.env.PORT) : 7777,
-  databaseUrl: process.env.URL,
-  JWTAcSecretKey: process.env.JWT_AC_SECRET_KEY,
-  JWTRfSecretKey: process.env.JWT_RF_SECRET_KEY,
-  JWTResetPasswordExpires: process.env.JWT_RESET_PASSWORD_EXPIRES,
-  JWTAcExpires: process.env.JWT_AC_EXPIRES,
-  JWTRfExpires: process.env.JWT_RF_EXPIRES,
+  JWTAcSecretKey: process.env.JWT_AC_SECRET_KEY || "ok",
+  JWTRfSecretKey: process.env.JWT_RF_SECRET_KEY || "ok1",
+  JWTResetPasswordExpires: process.env.JWT_RESET_PASSWORD_EXPIRES || "3m",
+  JWTAcExpires: process.env.JWT_AC_EXPIRES || "30d",
+  JWTRfExpires: process.env.JWT_RF_EXPIRES || "360d",
   fileSize: process.env.FILE_SIZE ? Number(process.env.FILE_SIZE) : 100, // mg
-  fileServerUrl: process.env.FILE_SERVER_URL,
-  chatCreateServerUrl: process.env.CHAT_CREAT_URL,
-  googleClientId: process.env.GOOGLE_CLIENT_ID,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
 };
